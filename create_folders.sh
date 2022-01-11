c7975ae9083bf84ee3b5e9c291500dd5ef0ad98f
MeleshK <melesh@knight22.com>
2022-01-11 17:33:47 +0000

    Added directory creation script

---
+++ create_folders.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir  Output/Stage1/
+mkdir rm Output/Stage2/Field_Data/
+mkdir rm Output/Stage2/Host_Data/
+mkdir rm Output/Stage2/Host_Info/
+mkdir rm Output/Stage2/Images/
+mkdir rm Output/Stage2/Summary/
